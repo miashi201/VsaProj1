02c93c8f1d7f84bd19dd7bc7137ebeb07b8b939e
miashi201 <40638877+miashi201@users.noreply.github.com>
2018-07-10 14:43:45 -0400

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -1,7 +1,7 @@
-# Name:
-# Date:
+# Name: Elizabeth Sims and Mia shines
+# Date: 7/10/18
 
-
+import random
 """ 
 proj 03: Guessing Game
 
@@ -12,4 +12,56 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 
 """
 
+#define variables
+first_loop = True
+points = 0
+
+#loop
+while first_loop == True:
+    second_loop = True
+    random_number = random.randint(1, 9)
+    guess_number = 0
+    print "I'm thinking of a number between 1 and 9. Can you guess my number?"
+    max_guesses = int(raw_input("How many guesses do you want? "))
+    while second_loop == True:
+        if guess_number == max_guesses:
+            print "You are out of guesses! The number was " + str(random_number) + "."
+            second_loop = False
+            print "You have " + str(points) + " points."
+            play_again = raw_input("Would you like to play again? (y/n)")
+            if play_again == "n":
+                first_loop = False
+                print "Game ended."
+        else:
+            user_input = raw_input("Enter a number, or 'exit' to end the game: ")
+            try:
+                int(user_input)
+                if int(user_input) < 1 or int(user_input) > 9:
+                    print "That is not a number between 1 and 9!"
+                elif int(user_input) > random_number:
+                    print "Your number is too high!"
+                    guess_number = guess_number + 1
+                elif int(user_input) < random_number:
+                    print "Your number is too low!"
+                    guess_number = guess_number + 1
+                elif int(user_input) == random_number:
+                    guess_number = guess_number + 1
+                    print "Congratulations, you guessed my number!", "You used", guess_number, "guesses."
+                    second_loop = False
+                    points = points + 1
+                    if points == 1:
+                        print "You have " + str(points) + " point."
+                    else:
+                        print "You have " + str(points) + " points."
+                    play_again = raw_input("Would you like to play again? (y/n)" )
+                    if play_again == "n":
+                        first_loop = False
+                        print "Game ended."
+            except ValueError:
+                if str(user_input) == "exit" or "Exit":
+                    print "Game ended."
+                    first_loop = False
+                    second_loop = False
+                else:
+                    print "That is not a valid answer!"
 
