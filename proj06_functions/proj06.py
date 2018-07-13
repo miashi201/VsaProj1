0d0557d5c36fbb074ebf94aaff1011ecfa6a7867
miashi201 <40638877+miashi201@users.noreply.github.com>
2018-07-13 15:42:50 -0500

    Add files via upload

--- proj06_functions/proj06.py
+++ proj06_functions/proj06.py
@@ -5,25 +5,40 @@
 
 # Part I
 
-def divisors(num):
-    """
-    Takes a number and returns all divisors of the number, ordered least to greatest
-    :param num: int
-    :return: list (int)
-    """
-
+# def divisors(num):
+#     # """
+#     # Takes a number and returns all divisors of the number, ordered least to greatest
+#     # :param num: int
+#     # :return: list (int)
+#     # """
+#
+#     d_list = []
+#     for i in range (1,num+1):
+#         if num%i == 0:
+#             d_list.append(i)
+#     return(d_list)
+#
+# print divisors(8)
+#     # Fill in the function and change the return statment.
+#
+#
+# def prime(num):
+#     # """
+#     # Takes a number and returns True if the number is prime, otherwise False
+#     # :param num: int
+#     # :return: bool
+#     # """
+#     lst2 = divisors(num)
+#     if lst2[0] == 1 and lst2[1] == num:
+#         return True
+#     else:
+#         return False
+#
+#
+#
+# print prime(7)
     # Fill in the function and change the return statment.
-    return 0
 
-def prime(num):
-    """
-    Takes a number and returns True if the number is prime, otherwise False
-    :param num: int
-    :return: bool
-    """
-
-    # Fill in the function and change the return statment.
-    return False
 
 
 # Part II:
@@ -38,9 +53,14 @@ def prime(num):
 #
 # Hint: Use a for loop and with an append function: list.append(letter)
 #
-# sentence_string = "Hello, my name is Monty Python."
+sentence_string = "Hello, my name is Monty Python."
 
 
+sentence_list = []
+s = sentence_string
+for letter in s:
+    sentence_list.append(letter)
+# print sentence_list
 
 
 
@@ -55,6 +75,8 @@ def prime(num):
 # m
 # y
 #  .... keeps going on from here.
+# for letter in s:
+#     print letter
 
 
 
@@ -63,21 +85,30 @@ def prime(num):
 # letter is 'b', print out the index of the current letter (should print out the
 # number 1).
 #
-# vowels = ['a', 'b', 'i', 'o', 'u', 'y']
-
-
+vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+counter = 0
+# for letter in vowels:
+#     if letter == "b":
+#      counter = counter + 1
+# print counter
 
 
 # 4: use the index found to change the list vowels so that the b is replaced with an e.
-
-
+#
+# for item in vowels:
+#     if item == "b":
+#         vowels[counter] = "e"
+#     counter = counter + 1
+# print vowels
 
 # 5: Loop through each letter in the sentence_string. For each letter, check to see if the
 #  number is in the vowels list. If the letter is in the vowels list, add one to a
 # counter. Print out the counter at the end of the loop. This counter should show how
 # many vowels are in sentence_string.
-
-
+# for letter in s:
+#     if letter in vowels:
+#         counter = counter + 1
+# print counter
 
 # 6: Make a new function called "vowelFinder" that will return a list of  the vowels
 # found in a list (no duplicates).The function's parameters should be "list" and "vowels."
@@ -86,8 +117,15 @@ def prime(num):
 #  Example:
 # vowelList = vowelFinder(sentence_list, vowels)
 # print vowelList
-
+v = "mama called the doctor on us"
 # ['a', 'e', 'i', 'o', 'y']
 
 def vowelFinder(sentence_list, vowels):
-    return []
+
+    vowel_list =[]
+    for letter in vowels:
+        if letter in sentence_list:
+            vowel_list.append(letter)
+
+    return [vowel_list]
+print vowelFinder(v, vowel)
